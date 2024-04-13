5a19e8861e889b63666411a4a47c48965b391609
traizooo <michal.owsiejew23@gmail.com>
2024-04-13 22:00:53 +0200

    Commit

--- front-end/components/login/Login.jsx
+++ front-end/components/login/Login.jsx
@@ -1,64 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import { URL } from "../../utils/constants";
+import toast, { Toaster } from "react-hot-toast";
 
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (!username || !password) {
+        toast.error(
+          "You need to provide username and password in order to login!"
+        )
+        return;
+      }
+
+      const response = await fetch(URL + '/login' , {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      })
+
+      if (!response.ok) {
+        toast.error("Wrong username or password!")
+      } else {
+        toast.success("Logged in successfully!")
+      }
+
+    } catch(error) {
+      console.error("There was an error " + error)
+    }
+
+  }
+
   return (
-    <div className="login-form-container">
-      <form class="login-form">
-        <p class="login-form-title">Sign in to your account</p>
-        <div class="login-input-container">
-          <input placeholder="Enter email" type="email" />
-          <span>
-            <svg
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </span>
-        </div>
-        <div class="login-input-container">
-          <input placeholder="Enter password" type="password" />
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="login-form-container">
+        <form className="login-form">
+          <p className="login-form-title">Sign in to your account</p>
+          <div className="login-input-container">
+            <input placeholder="Enter username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <span>
+              <svg
+                strokeWidth="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <div className="login-input-container">
+            <input placeholder="Enter password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
-          <span>
-            <svg
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-              <path
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </span>
-        </div>
-        <button class="login-submit" type="submit">
-          Sign in
-        </button>
+            <span>
+              <svg
+                strokeWidth="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <path
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <button className="login-submit" onClick={onClick}>
+            Sign in
+          </button>
 
-        <p class="login-signup-link">
-          No account?
-          <a href="/register">Sign up</a>
-        </p>
-      </form>
-    </div>
+          <p className="login-signup-link">
+            No account?
+            <a href="/register">Sign up</a>
+          </p>
+        </form>
+      </div>
+    </>
   );
 };
 
--- front-end/components/register/Register.jsx
+++ front-end/components/register/Register.jsx
@@ -1,104 +1,184 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
+import { URL } from "../../utils/constants";
+import toast, { Toaster } from "react-hot-toast";
 
 const Register = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [retypePassword, setRetypePassword] = useState("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const onClick = async (e) => {
+    e.preventDefault()
+    try {
+      if (!username || !email || !password || !retypePassword) {
+        toast.error(
+          "You need to fill in all input fields in order to register!"
+        );
+        return;
+      }
+      if (!validateEmail(email)) {
+        toast.error("Provide vaild email")
+        return;
+      }
+
+      if (password != retypePassword) {
+        toast.error("Passwords must match!");
+        return;
+      }
+
+      const response = await fetch(URL + '/register', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          username: username,
+          password: password
+        })
+      })
+
+      if (!response.ok) {
+        toast.error("The account with provided email or username already exists!")
+      } else {
+        toast.success("User registered successfully!");
+      }
+
+    } catch(error) {
+      console.error("There was an error " + error)
+    }
+  }
+  
   return (
-    <div className="register-form-container">
-      <form class="register-form">
-        <p class="register-form-title">Register your account</p>
-        <div class="register-input-container">
-          <input placeholder="Enter email" type="email" />
-          <span>
-            <svg
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </span>
-        </div>
-        <div class="register-input-container">
-          <input placeholder="Enter username" type="email" />
-          <span>
-            <svg
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </span>
-        </div>
-        <div class="register-input-container">
-          <input placeholder="Enter password" type="password" />
-          <span>
-            <svg
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-              <path
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </span>
-        </div>
-        <div class="register-input-container">
-          <input placeholder="Re-type password" type="password" />
-          <span>
-            <svg
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-              <path
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                stroke-width="2"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </span>
-        </div>
-        <button class="register-submit" type="submit">
-          Sign up
-        </button>
-        <p class="register-signup-link">
-          Already have an account?
-          <a href="/login">Log in</a>
-        </p>
-      </form>
-    </div>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="register-form-container">
+        <form className="register-form">
+          <p className="register-form-title">Register your account</p>
+          <div className="register-input-container">
+            <input
+              placeholder="Enter email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <span>
+              <svg
+                strokeLinecap="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <div className="register-input-container">
+            <input
+              placeholder="Enter username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <span>
+              <svg
+                strokeWidth="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <div className="register-input-container">
+            <input
+              placeholder="Enter password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span>
+              <svg
+                strokeWidth="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <path
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <div className="register-input-container">
+            <input
+              placeholder="Re-type password"
+              type="password"
+              value={retypePassword}
+              onChange={(e) => setRetypePassword(e.target.value)}
+            />
+            <span>
+              <svg
+                strokeWidth="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <path
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <button className="register-submit" onClick={onClick}>
+            Sign up
+          </button>
+          <p className="register-signup-link">
+            Already have an account?
+            <a href="/login">Log in</a>
+          </p>
+        </form>
+      </div>
+    </>
   );
 }
 
